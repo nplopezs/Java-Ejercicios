feec97d3349e1ce002331f64439aff7d0249dbf9
NatLop <nplopez_s@hotmail.com>
2023-03-09 19:16:08 -0600

    Ejercicios Java

---
+++ Practica.java
@@ -0,0 +1,82 @@
+import java.text.DateFormat;
+public class Practica {
+	
+	//Funciones
+	
+	static void primerMetodo() { // Si le ponemos un argumento le tienes que especificar el tipo de dato
+		System.out.println("Holaaa");
+	}
+	
+	//Ejercicio 1
+	
+	//Ejercicio 2
+	static void circulo (double radio) {
+		double perimetro = 0;
+		double area = 0;
+		perimetro = 2*Math.PI*radio;
+		area = Math.PI * (radio*radio);
+		DecimalFormat formato1 = new DecimalFormat("#.00");
+		System.out.println("Radio:"+formato1.format(radio));
+		System.out.println("Perimetro:"+ formato1.format(perimetro));
+		System.out.println("Area:"+formato1.format(area));
+	}
+	
+	static void horaSistema() {
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		Date date = new Date();
+		System.out.println("Hora actual: " + dateFormat.format(date));
+	}
+	
+	static void changeSec(double seg) {
+		double horas;
+		double minutos;
+		horas = seg/3600;
+		minutos = seg/60;
+		System.out.println(seg+"segundos = "+horas+"horas = " + minutos+"minutos = "+seg+"segundos");
+	}
+	
+	static void volEsfera(double radio) {
+		double volumen;
+		DecimalFormat formato1 = new DecimalFormat("#.00");
+		volumen = (4/3)*Math.PI*(Math.pow(radio, 3));
+		System.out.println("Volumen: "+formato1.format(volumen));
+	}
+	
+	static void volCono(double radio,double altura) {
+		double volumen;
+		DecimalFormat formato1 = new DecimalFormat("#.00");
+		volumen = (1/3)*altura*Math.PI*(radio*radio);
+		System.out.println("Volumen: "+formato1.format(volumen));
+	}
+	
+	static void areaSuper(double lado) {
+		double area;
+		DecimalFormat formato1 = new DecimalFormat("#.00");
+		area = 6*(lado*lado);
+		System.out.println("Area superficial: "+formato1.format(area));
+	}
+	
+	static boolean bisiesto(int year) {
+	    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+	}
+
+	
+	public static void main(String[] args) {
+		
+		primerMetodo();
+		
+		circulo(3.0);
+		System.out.println("\n");
+		horaSistema();
+		System.out.println("\n");
+		changeSec(3600);
+		System.out.println("\n");
+		volEsfera(4.0);
+		System.out.println("\n");
+		volCono(3.0,10.0);
+		System.out.println("\n");
+		System.out.println(bisiesto(2024));
+		
+	}
+
+}
